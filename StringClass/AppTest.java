4135b1880e0e010d93f193ce9b1457e45f00bb08
Rotha SAMON <samonrotha@gmail.com>
2025-06-03 19:03:12 +0700

    My code - If pass right output success, else

--- StringClass/AppTest.java
+++ StringClass/AppTest.java
@@ -8,27 +8,31 @@ public class AppTest {
         String pass;
         int count = 1;
         boolean flag = false;
-        
+
         do { 
             System.out.print("Enter username: ");
             user = scanner.next();
             System.out.print("Enter password:");
             pass = scanner.next();
             if(count == 3){
-                flag = true;
+                // flag = true;
+                // break;
+                // System.out.println("Sorry see you next time");
                 break;
             }
             
             count++;
         } while (!(user.equals("dara") && pass.equals("dara@123")));
         
-        if(flag){
-            System.out.println("Sorry see you next time");
-        }else{
-            System.out.println("Welcome Dara");
+        if(user.equals("dara") && pass.equals("dara@123")) {
+            System.out.println("Success!");
+        }else {
+            System.out.println("Wrong username or password");
         }
-
-      
-
+        // if(flag){
+        //     System.out.println("Sorry see you next time");
+        // }else{
+        //     System.out.println("Welcome Dara");
+        // }
     }
 }
